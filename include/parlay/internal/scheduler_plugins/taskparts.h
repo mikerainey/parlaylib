0c2f9f4abbe4cd1b3ccbc389cb29b866bb0d70e7
mikerainey <bionic.oldsmobile@gmail.com>
2021-09-16 20:43:43 -0500

    got first benchmark to run

--- include/parlay/internal/scheduler_plugins/taskparts.h
+++ include/parlay/internal/scheduler_plugins/taskparts.h
@@ -3,46 +3,81 @@
 #if defined(PARLAY_TASKPARTS)
 
 #include <taskparts/benchmark.hpp>
+#include <algorithm>
 
 namespace parlay {
 
 // IWYU pragma: private, include "../../parallel.h"
 
+bool taskparts_launched = false;
+
 inline size_t num_workers() { return taskparts::perworker::nb_workers(); }
 inline size_t worker_id() { return taskparts::perworker::my_id(); }
 
-using taskparts_scheduler = minimal_scheduler<bench_stats, bench_logging, bench_elastic>;
+using taskparts_scheduler = taskparts::minimal_scheduler<taskparts::bench_stats, taskparts::bench_logging, taskparts::bench_elastic>;
 
 template <typename Lf, typename Rf>
 inline void par_do(Lf left, Rf right, bool) {
-  taskparts::fork2join<taskparts_scheduler>(left, right);
-}
-
-template <typename F>
-inline void parallel_for(size_t start, size_t end, F f,
-                         long granularity,
-			 bool) {
-  if ((end - start) <= granularity)
-    for (size_t i=start; i < end; i++) f(i);
-  else {
-    size_t n = end-start;
-    size_t mid = (start + (9*(n+1))/16);
-    par_do([&] {
-      parallel_for(start, mid, f, granularity);
-    }, [&] {
-      parallel_for(mid, end, f, granularity);
-    });
+  if (taskparts_launched) {
+    taskparts::fork2join<Lf, Rf, taskparts_scheduler>(left, right);
+  } else {
+    left();
+    right();
   }
 }
 
-static
-bool taskparts_launched = false;
+template <typename F>
+size_t get_granularity(size_t start, size_t end, F f) {
+  size_t done = 0;
+  size_t sz = 1;
+  int ticks = 0;
+  do {
+    sz = std::min(sz, end - (start + done));
+    auto tstart = std::chrono::high_resolution_clock::now();
+    for (size_t i = 0; i < sz; i++) f(start + done + i);
+    auto tstop = std::chrono::high_resolution_clock::now();
+    ticks = static_cast<int>((tstop - tstart).count());
+    done += sz;
+    sz *= 2;
+  } while (ticks < 1000 && done < (end - start));
+  return done;
+}
+
+template <typename F>
+void parfor_(size_t start, size_t end, F f, size_t granularity,
+	     bool conservative) {
+  if ((end - start) <= granularity)
+    for (size_t i = start; i < end; i++) f(i);
+  else {
+    size_t n = end - start;
+    // Not in middle to avoid clashes on set-associative
+    // caches on powers of 2.
+    size_t mid = (start + (9 * (n + 1)) / 16);
+    par_do([&]() { parfor_(start, mid, f, granularity, conservative); },
+	   [&]() { parfor_(mid, end, f, granularity, conservative); },
+	   conservative);
+  }
+}
+
+  
+template <typename F>
+inline void parallel_for(size_t start, size_t end, F f,
+                         long granularity,
+			 bool conservative) {
+  if (end <= start) return;
+  if (granularity == 0) {
+    size_t done = get_granularity(start, end, f);
+    granularity = std::max(done, (end - start) / (128 * num_workers()));
+    parfor_(start + done, end, f, granularity, conservative);
+  } else
+    parfor_(start, end, f, granularity, conservative);
+}
 
 template <typename Benchmark>
 auto benchmark_taskparts(const Benchmark& benchmark) {
-  taskparts::benchmark_nativefj([&] (auto sched) {
+  taskparts::benchmark_nativeforkjoin([&] (auto sched) {
     taskparts_launched = true;
-    benchmark();
+    benchmark(sched);
   });
 }
 
---
+++ taskparts/shell.nix
@@ -0,0 +1,50 @@
+# Creates an environment prepared for taskparts.
+#
+# By default, the environment uses clang/llvm instead of GCC. To
+# use GCC instead, use the following:
+#   $ nix-shell --arg stdenv '(import <nixpkgs> {}).stdenv'
+#
+# If you don't want to use jemalloc:
+#   $ nix-shell --arg jemalloc null
+# If you don't want to use hwloc:
+#   $ nix-shell --arg hwloc null
+# Note: if you use hwloc, then the taskparts scheduler will *not* use SMT.
+# For full debugging:
+#   $ nix-shell --arg stdenv '(import <nixpkgs> {}).stdenv' --arg jemalloc null --arg hwloc null
+
+{ pkgs   ? import <nixpkgs> {},
+  stdenv ? pkgs.clangStdenv,
+  hwloc ? pkgs.hwloc,
+  jemalloc ? pkgs.jemalloc450,
+  valgrind ? pkgs.valgrind
+}:
+
+stdenv.mkDerivation rec {
+  name = "taskparts-dev";
+
+  buildInputs =
+    [ ]
+    ++ (if hwloc == null then [] else [ hwloc ])
+    ++ (if jemalloc == null then [] else [ jemalloc ])
+    ++ (if valgrind == null then [] else [ valgrind ]);
+
+  shellHook =
+    let
+      jemallocCfg = 
+        if jemalloc == null then "" else
+          ''export LD_PRELOAD="${jemalloc}/lib/libjemalloc.so"'';
+      hwlocCfg =
+        if hwloc == null then "" else ''
+            export HWLOC_INCLUDE_PREFIX="-DTASKPARTS_HAVE_HWLOC -I${hwloc.dev}/include/"
+            export HWLOC_LIBRARY_PREFIX="-L${hwloc.lib}/lib/ -lhwloc"
+            # the line below is needed because otherwise taskparts may request more
+            # workers than there are cores, which would be incompatible with the
+            # per-core pinning
+            export TASKPARTS_NUM_WORKERS=$( ${hwloc}/bin/hwloc-ls|grep Core|wc -l )
+        '';
+    in
+    ''
+    ${hwlocCfg}
+    ${jemallocCfg}
+    '';
+}
