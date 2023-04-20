672f864680260501986bc6f172d0671c24923965
mikerainey <bionic.oldsmobile@gmail.com>
2023-04-20 18:30:01 -0400

    updated to support the most recent taskparts

--- include/parlay/internal/scheduler_plugins/taskparts.h
+++ include/parlay/internal/scheduler_plugins/taskparts.h
@@ -3,7 +3,8 @@
 #if defined(PARLAY_TASKPARTS)
 
 // clang++ -I../include -std=c++17 -DPARLAY_TASKPARTS -fno-stack-protector -fno-asynchronous-unwind-tables -fomit-frame-pointer -DTASKPARTS_X64 -DTASKPARTS_POSIX word_counts.cpp -o wc
-#include "/home/rainey/Work/website/blog/microtaskparts.cpp"
+
+#include "/home/rainey/Work/successor/src/taskparts.cpp"
 #include <algorithm>
 
 namespace parlay {
@@ -14,12 +15,15 @@ inline size_t num_workers() { return taskparts::get_nb_workers(); }
 inline size_t worker_id() { return taskparts::get_my_id(); }
 
 template <typename Lf, typename Rf>
-inline void par_do(Lf left, Rf right, bool) {
+inline void par_do(Lf&& left, Rf&& right, bool) {
+  static_assert(std::is_invocable_v<Lf&&>);
+  static_assert(std::is_invocable_v<Rf&&>);
   taskparts::fork2join<Lf, Rf>(left, right);
 }
 
 template <typename F>
 size_t get_granularity(size_t start, size_t end, F f) {
+  static_assert(std::is_invocable_v<F&, size_t>);
   size_t done = 0;
   size_t sz = 1;
   int ticks = 0;
@@ -35,36 +39,34 @@ size_t get_granularity(size_t start, size_t end, F f) {
   return done;
 }
 
+size_t override_granularity = 0;
+
 template <typename F>
-void parfor_(size_t start, size_t end, F f, size_t granularity,
-	     bool conservative) {
-  if ((end - start) <= granularity)
-    for (size_t i = start; i < end; i++) f(i);
-  else {
-    size_t n = end - start;
-    // Not in middle to avoid clashes on set-associative
-    // caches on powers of 2.
-    size_t mid = (start + (9 * (n + 1)) / 16);
-    par_do([&]() { parfor_(start, mid, f, granularity, conservative); },
-	   [&]() { parfor_(mid, end, f, granularity, conservative); },
-	   conservative);
+void parallel_for_rec(size_t start, size_t end, F&& f, long granularity) {
+  if ((end - start) <= granularity) {
+    for (size_t i = start; i < end; i++) {
+      f(i);
+    }
+  } else {
+    size_t n = end-start;
+    size_t mid = (start + (9*(n+1))/16);
+    par_do([&] { parallel_for_rec(start, mid, f, granularity); },
+	   [&] { parallel_for_rec(mid, end, f, granularity); }, false);
   }
 }
 
-size_t override_granularity = 0;
-  
 template <typename F>
-inline void parallel_for(size_t start, size_t end, F f,
-                         long granularity,
-			 bool conservative) {
+inline void parallel_for(size_t start, size_t end, F&& f, long granularity, bool) {
+  static_assert(std::is_invocable_v<F&, size_t>);
   if (end <= start) return;
   granularity = override_granularity == 0 ? granularity : override_granularity;
   if (granularity == 0) {
     size_t done = get_granularity(start, end, f);
     granularity = std::max(done, (end - start) / (128 * num_workers()));
-    parfor_(start + done, end, f, granularity, conservative);
-  } else
-    parfor_(start, end, f, granularity, conservative);
+    parallel_for_rec(start + done, end, f, granularity);
+  } else {
+    parallel_for_rec(start, end, f, granularity);
+  }
 }
 
 }  // namespace parlay
